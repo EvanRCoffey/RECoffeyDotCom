31433291d543df90e864b1cf9904f2c99fda9b40
REvanCof <rogerecoffey@gmail.com>
2017-04-20 15:43:50 -0500

    added public_html folder for use with godaddy/filezilla

--- controllers/controller.js
+++ controllers/controller.js
@@ -5,27 +5,27 @@ var model = require('../models/model.js');
 
 //Home
 router.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "/../public/index.html"));
+  res.sendFile(path.join(__dirname, "/../public_html/index.html"));
 });
 
 //Bio
 router.get("/bio", function(req, res) {
-  res.sendFile(path.join(__dirname, "/../public/bio.html"));
+  res.sendFile(path.join(__dirname, "/../public_html/bio.html"));
 });
 
 //Contact
 router.get("/contact", function(req, res) {
-  res.sendFile(path.join(__dirname, "/../public/contact.html"));
+  res.sendFile(path.join(__dirname, "/../public_html/contact.html"));
 });
 
 //Portfolio
 router.get("/portfolio", function(req, res) {
-  res.sendFile(path.join(__dirname, "/../public/portfolio.html"));
+  res.sendFile(path.join(__dirname, "/../public_html/portfolio.html"));
 });
 
 // If no matching route is found default to home
 router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "/../public/index.html"));
+  res.sendFile(path.join(__dirname, "/../public_html/index.html"));
 });
 
 // Export routes for server.js to use.
